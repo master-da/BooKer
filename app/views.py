21801e75ee1adaf60b97d3f22a5a98daab54ce83
Bholanath Das Niloy <drakensang47@gmail.com>
2023-05-12 17:41:00 +0600

    Linked stuff around

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app, render_template, request, hotelApi
 
-@app.route("/home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     locations = []
     if request.method == "POST":
@@ -10,3 +10,14 @@ def index():
 
     return render_template("home.html", locations=locations)
 
+@app.route("/hotel")
+def hotels():
+    return render_template("hotel.html", name="Hotel")
+
+@app.route("/signup")
+def signup():
+    return render_template("sign_up.html", name="Signup")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", name="Login")
