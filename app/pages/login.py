8206ba3592b8562986060b2d5e4363c43b53be98
master-da <mdnokib2000@gmail.com>
2023-05-14 14:15:09 +0600

    pullin

--- app/pages/login.py
+++ app/pages/login.py
@@ -13,6 +13,7 @@ class Login:
     
     def handlePostReq(self):
         if 'to_signup' in request.form:
+<<<<<<< HEAD
             print("to signup")
             return redirect(url_for('signup'))
         elif 'signin' in request.form:
@@ -25,3 +26,10 @@ class Login:
                 login_user(user)
                 return redirect(url_for('home'))
             return render_template("login.html")
+=======
+            return redirect(url_for('signup'))
+        elif 'signin' in request.form:
+            if 'email' not in request.form or 'pass' not in request.form:
+                return render_template("login.html", error="Please enter both email and password")
+    
+>>>>>>> 20930b7b288b9f605bc26103d5cf6c3efe8bf694
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from app import app, render_template, request, hotelApi, login_required
+from app import app, render_template, request, hotelApi, redirect, url_for
 from app.pages.login import Login
 from app.pages.signup import Signup
 from app.pages.home import Home
 from app.pages.hotel import Hotel
+from app.pages.profile import Profile
+from app.pages.itinerary import Itinerary
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -23,3 +25,11 @@ def signup():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return Login().serve()
+
+@app.route("/profile", methods=["GET", "POST"])
+def profile():
+    return Profile().serve()
+
+@app.route("/itinerary", methods=["GET", "POST"])
+def itinerary():
+    return Itinerary().serve()
